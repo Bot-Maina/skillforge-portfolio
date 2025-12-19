3c55bff648b62eb857a2cfa131c0ee8a50e40b65
Bot-Maina <86517595+Bot-Maina@users.noreply.github.com>
2025-12-19 23:50:50 +0300

    R

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,31 +1,7 @@
-import Navigation from "@/components/portfolio/Navigation";
-import TitleSection from "@/components/portfolio/TitleSection";
-import IdentitySection from "@/components/portfolio/IdentitySection";
-import DeclarationSection from "@/components/portfolio/DeclarationSection";
-import CaseStudiesSection from "@/components/portfolio/CaseStudiesSection";
-import IntroLetterSection from "@/components/portfolio/IntroLetterSection";
-import ResumeSection from "@/components/portfolio/ResumeSection";
-import PriorLearningSection from "@/components/portfolio/PriorLearningSection";
-import GallerySection from "@/components/portfolio/GallerySection";
-import RPLCriteriaSection from "@/components/portfolio/RPLCriteriaSection";
-import Footer from "@/components/portfolio/Footer";
-
 const Index = () => {
   return (
-    <div className="min-h-screen bg-background">
-      <Navigation />
-      <main>
-        <TitleSection />
-        <IdentitySection />
-        <DeclarationSection />
-        <CaseStudiesSection />
-        <IntroLetterSection />
-        <ResumeSection />
-        <PriorLearningSection />
-        <GallerySection />
-        <RPLCriteriaSection />
-      </main>
-      <Footer />
+    <div>
+      <h1>Hello World</h1>
     </div>
   );
 };
